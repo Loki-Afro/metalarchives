a99774b27ca960d22e6ba75ea1cadfe9e6cc0d1f
Zarathustra <Zarathustra@192.168.1.146>
2013-05-01 17:23:17 +0200

    clean

--- metalarchivesAPI/src/main/java/de/loki/metallum/core/util/net/downloader/HTMLDownloader.java
+++ metalarchivesAPI/src/main/java/de/loki/metallum/core/util/net/downloader/HTMLDownloader.java
@@ -8,21 +8,30 @@ import de.loki.metallum.core.util.net.downloader.interfaces.IHTMLDowloader;
 
 public final class HTMLDownloader extends AbstractDownloader implements IHTMLDowloader {
 
-	protected HTMLDownloader(String urlString) {
+	protected HTMLDownloader(final String urlString) {
 		super(urlString);
 	}
 
 	@Override
 	public final String call() throws Exception {
-		InputStream in = getDownloadEntity().getContent();
-		BufferedReader rd = new BufferedReader(new InputStreamReader(in, HTML_CHARSET));
+		InputStream in = null;
+		BufferedReader rd = null;
 		StringBuilder htmlStringBuilder = new StringBuilder();
-		String line;
-		while ((line = rd.readLine()) != null) {
-			htmlStringBuilder.append(line);
+		try {
+			in = getDownloadEntity().getContent();
+			rd = new BufferedReader(new InputStreamReader(in, HTML_CHARSET));
+			String line;
+			while ((line = rd.readLine()) != null) {
+				htmlStringBuilder.append(line);
+			}
+		} finally {
+			if (rd != null) {
+				rd.close();
+			}
+			if (in != null) {
+				in.close();
+			}
 		}
-		rd.close();
-		in.close();
 
 		return htmlStringBuilder.toString();
 	}
