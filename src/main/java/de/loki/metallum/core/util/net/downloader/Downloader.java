69929c32dd4e9a063fc8f311674f7370b0c59f83
Phillip Wirth <phillip.wirth.dev@gmail.com>
2016-03-30 21:30:31 +0200

    removed unused class

--- src/main/java/de/loki/metallum/core/util/net/downloader/Downloader.java
+++ src/main/java/de/loki/metallum/core/util/net/downloader/Downloader.java
@@ -6,10 +6,19 @@ import org.slf4j.LoggerFactory;
 
 import java.awt.image.BufferedImage;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Downloader {
 
-	private static Logger logger = LoggerFactory.getLogger(Downloader.class);
+	private final static ThreadPoolExecutor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(4, 10, 5L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>()) {
+		{
+			allowCoreThreadTimeOut(true);
+		}
+	};
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Downloader.class);
 
 	public static BufferedImage getImage(final String urlString) throws ExecutionException {
 		return get(urlString, new ImageDownloader(urlString));
@@ -21,10 +30,13 @@ public class Downloader {
 
 	private static <T> T get(final String request, final IContentDownloader<T> downloader) throws ExecutionException {
 		try {
-			return ThreadExecuter.getInstance().submit(downloader).get();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-			logger.error("Failed to download: " + request, e);
+			LOGGER.debug("Task count: " + THREAD_POOL_EXECUTOR.getTaskCount());
+			LOGGER.debug("Queue size: " + THREAD_POOL_EXECUTOR.getQueue().size());
+			LOGGER.debug("completedTaskCount: " + THREAD_POOL_EXECUTOR.getCompletedTaskCount());
+			LOGGER.debug("Remaining Queue Capacity: " + THREAD_POOL_EXECUTOR.getQueue().remainingCapacity());
+			return THREAD_POOL_EXECUTOR.submit(downloader).get();
+		} catch (final InterruptedException e) {
+			LOGGER.error("Failed to download: " + request, e);
 		}
 		return null;
 	}
